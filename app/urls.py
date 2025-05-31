111d4fdb2bd03d11e4fd90f8b64af8fe51d46d0d
Leshik1305 <realleshik1305@gmail.com>
2025-05-31 21:41:39 +0300

    add users and users-templates

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -22,14 +23,17 @@ from app import settings
 from main import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace="main"), ),
-    path('catalog/', include('goods.urls', namespace="catalog") )
-
+    path("admin/", admin.site.urls),
+    path(
+        "",
+        include("main.urls", namespace="main"),
+    ),
+    path("catalog/", include("goods.urls", namespace="catalog")),
+    path("user/", include("users.urls", namespace="user")),
 ]
 
 if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
     ]
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
